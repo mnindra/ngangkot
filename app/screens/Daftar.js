4f8540ccc50a0e6f2c02b2739c3a63b14cd86bff
mnindra <mnindrazaka@gmail.com>
2017-08-28 22:15:54 +0700

    daftar dengan akun baru

--- app/screens/Daftar.js
+++ app/screens/Daftar.js
@@ -18,13 +18,57 @@ import {
 import { StyleSheet, Image } from 'react-native';
 import getTheme from '../../native-base-theme/components';
 import material from '../../native-base-theme/variables/material';
+import firebase from '../config/firebase';
 
 export default class Daftar extends Component {
 
+  constructor (props) {
+    super(props);
+    this.state = {
+      nama: "",
+      email: "",
+      alamat: "",
+      telp: "",
+      password: "",
+      konfirmasi_password: ""
+    }
+  }
+
+  daftar () {
+    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then(() => {
+      let user = firebase.auth().currentUser;
+      firebase.database().ref('penumpang/' + user.uid).set({
+        id_penumpang: user.uid,
+        nama: this.state.nama,
+        email: this.state.email,
+        alamat: this.state.alamat,
+        telp: this.state.telp,
+        password: this.state.password,
+        blokir: 0,
+        lokasi: {
+          latitude: 0,
+          longitude: 0
+        },
+        online: 1,
+        tanggal: new Date()
+      });
+      this.resetInput();
+    });
+  }
+
+  resetInput() {
+    this.setState({
+      nama: "",
+      email: "",
+      alamat: "",
+      telp: "",
+      password: "",
+      konfirmasi_password: ""
+    });
+  }
+
   render() {
-
     const {goBack} = this.props.navigation;
-
     return (
       <StyleProvider style={getTheme(material)}>
         <Container style={styles.container}>
@@ -42,34 +86,61 @@ export default class Daftar extends Component {
                 <Form style={styles.form}>
                   <Item floatingLabel>
                     <Icon name="person" style={{color:'#4c4c4c'}}/>
-                    <Input placeholder="Nama" />
+                    <Input
+                      placeholder="Nama"
+                      value={this.state.nama}
+                      onChangeText={(text) => this.setState({nama: text})} />
                   </Item>
+
                   <Item floatingLabel>
                     <Icon name="email" style={{color:'#4c4c4c'}}/>
-                    <Input placeholder="Email" />
+                    <Input
+                      placeholder="Email"
+                      value={this.state.email}
+                      onChangeText={(text) => this.setState({email: text})}
+                      keyboardType={"email-address"}/>
                   </Item>
+
                   <Item floatingLabel>
                     <Icon name="location-on" style={{color:'#4c4c4c'}}/>
-                    <Input placeholder="Alamat" />
+                    <Input
+                      placeholder="Alamat"
+                      value={this.state.alamat}
+                      onChangeText={(text) => this.setState({alamat: text})} />
                   </Item>
+
                   <Item floatingLabel>
                     <Icon name="smartphone" style={{color:'#4c4c4c'}}/>
-                    <Input placeholder="Nomor Telepon" />
+                    <Input
+                      placeholder="Nomor Telepon"
+                      value={this.state.telp}
+                      onChangeText={(text) => this.setState({telp: text})}
+                      keyboardType={"numeric"}/>
+
                   </Item>
                   <Item floatingLabel>
                     <Icon name="lock" style={{color:'#4c4c4c'}}/>
-                    <Input placeholder="Password" />
+                    <Input
+                      placeholder="Password"
+                      value={this.state.password}
+                      onChangeText={(text) => this.setState({password: text})}
+                      secureTextEntry={true}/>
                   </Item>
 
                   <Item floatingLabel>
                     <Icon name="lock" style={{color:'#4c4c4c'}}/>
-                    <Input placeholder="Konfirmasi Password" />
+                    <Input
+                      placeholder="Konfirmasi Password"
+                      value={this.state.konfirmasi_password}
+                      onChangeText={(text) => this.setState({konfirmasi_password: text})}
+                      secureTextEntry={true} />
                   </Item>
                 </Form>
 
                 <Button
                   success
                   block
+                  onPress={() => this.daftar()}
                   style={{marginBottom: 10}}>
                   <Text>Daftar</Text>
                 </Button>
