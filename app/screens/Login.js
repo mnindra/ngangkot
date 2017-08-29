2625414e0c3d55b93d5aa1cfb87fddbdea370609
mnindra <mnindrazaka@gmail.com>
2017-08-30 04:11:18 +0700

    mekanisme login

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,91 +1,174 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ValidationComponent from 'react-native-form-validator';
 import {
-  Container,
-  Content,
-  Button,
-  Text,
-  Form,
-  Item,
-  Input,
-  Card,
-  CardItem,
-  Label,
-  H2,
-  Icon,
-  Thumbnail,
-  StyleProvider
+    Container,
+    Content,
+    Button,
+    Text,
+    Form,
+    Item,
+    Input,
+    Card,
+    H2,
+    Icon,
+    StyleProvider
 } from 'native-base';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image, Alert } from 'react-native';
+import ErrorLabel from '../components/ErrorLabel';
 import getTheme from '../../native-base-theme/components';
 import material from '../../native-base-theme/variables/material';
+import firebase from '../config/firebase';
 
-export default class Login extends Component {
+export default class Login extends ValidationComponent {
 
-  render() {
-    const { navigate } = this.props.navigation;
-    return (
-      <StyleProvider style={getTheme(material)}>
-      <Container style={styles.container}>
-        <Content padder>
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            errors: {}
+        };
+    }
 
-          <Image style={styles.logo} source={require('../images/logo.png')} />
-          <H2 style={styles.title}>
-            Ngangkot
-          </H2>
+    login() {
+        this.validasiForm();
 
-          <Card style={styles.card} transparent>
-            <Content padder>
-              <Text style={styles.formText}>Login Untuk Melanjutkan</Text>
+        if (this.isFormValid()) {
+            firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then(() => {
+                this.resetInput();
+              // this.props.navigation.navigate('');
+                Alert.alert("berhasil login");
+            }).catch((error) => {
+                Alert.alert("kesalahan", error.code);
+                switch (error.code) {
+                    case "auth/network-request-failed":
+                        Alert.alert("Koneksi Gagal", "Cek koneksi internet anda");
+                        break;
+                    case "auth/user-not-found":
+                        this.setState({
+                            password: "",
+                            errors: {
+                                email: "email tidak ditemukan"
+                            }
+                        });
+                        break;
+                    case "auth/wrong-password":
+                        this.setState({
+                            password: "",
+                            errors: {
+                                password: "password tidak cocok"
+                            }
+                        });
+                        break;
+                    default:
+                        Alert.alert("Terjadi Kesalahan", "Kesalahan tidak diketahui");
+                }
+            });
+        }
+    }
 
-              <Form style={styles.form}>
-                <Item floatingLabel>
-                  <Icon name="email" style={{color:'#4c4c4c'}}/>
-                  <Input placeholder="Email" />
-                </Item>
-                <Item floatingLabel>
-                  <Icon name="lock" style={{color:'#4c4c4c'}}/>
-                  <Input placeholder="Password" />
-                </Item>
-              </Form>
+    resetInput() {
+      this.setState({
+         email: "",
+         password: ""
+      });
+    }
 
-              <Button block style={styles.loginButton}><Text>Login</Text></Button>
-              <Button success block onPress={() => navigate('Daftar')}><Text>Daftar</Text></Button>
-            </Content>
-          </Card>
-        </Content>
-      </Container>
-      </StyleProvider>
-    );
-  }
+    validasiForm() {
+        this.validate({
+            email: {required: true, email: true},
+            password: {required: true}
+        });
+
+        let errors = {};
+        let errors_string = this.getErrorMessages().split("\n");
+        errors_string.forEach((error) => {
+            for (let key in this.state) {
+                if (error.indexOf(key) !== -1) {
+                    errors[key] = error;
+                }
+            }
+        });
+
+        this.setState({errors});
+    }
+
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+          <StyleProvider style={getTheme(material)}>
+            <Container style={styles.container}>
+              <Content padder>
+
+                <Image style={styles.logo} source={require('../images/logo.png')} />
+                <H2 style={styles.title}>
+                  Ngangkot
+                </H2>
+
+                <Card style={styles.card} transparent>
+                  <Content padder>
+                    <Text style={styles.formText}>Login Untuk Melanjutkan</Text>
+
+                    <Form style={styles.form}>
+                      <Item floatingLabel error={this.isFieldInError('email')}>
+                        <Icon name="email" style={{color:'#4c4c4c'}}/>
+                        <Input
+                          placeholder="Email"
+                          value={this.state.email}
+                          onChangeText={(text) => this.setState({email: text})}/>
+                      </Item>
+                      <ErrorLabel error={this.state.errors.email} />
+
+                      <Item floatingLabel error={this.isFieldInError('password')}>
+                        <Icon name="lock" style={{color:'#4c4c4c'}}/>
+                        <Input
+                          placeholder="Password"
+                          value={this.state.password}
+                          secureTextEntry={true}
+                          onChangeText={(text) => this.setState({password: text})}/>
+                      </Item>
+                      <ErrorLabel error={this.state.errors.password} />
+
+                    </Form>
+
+                    <Button block style={styles.loginButton} onPress={() => this.login()}><Text>Login</Text></Button>
+                    <Button success block onPress={() => navigate('Daftar')}><Text>Daftar</Text></Button>
+                  </Content>
+                </Card>
+              </Content>
+            </Container>
+          </StyleProvider>
+        );
+    }
 }
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#00BCD4'
-  },
-  title: {
-    color: '#fff',
-    textAlign: 'center',
-    marginBottom: 20,
-    fontSize: 30,
-    height: 35
-  },
-  form: {
-    marginBottom: 20
-  },
-  formText: {
-    color: '#5f5f5f',
-    textAlign: 'center'
-  },
-  loginButton: {
-    marginBottom: 10,
-    backgroundColor: '#5c98ed'
-  },
-  logo: {
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginTop: 20
-  },
-  card: {
-    paddingTop: 20
-  }
+    container: {
+        backgroundColor: '#00BCD4'
+    },
+    title: {
+        color: '#fff',
+        textAlign: 'center',
+        marginBottom: 20,
+        fontSize: 30,
+        height: 35
+    },
+    form: {
+        marginBottom: 20
+    },
+    formText: {
+        color: '#5f5f5f',
+        textAlign: 'center'
+    },
+    loginButton: {
+        marginBottom: 10,
+        backgroundColor: '#5c98ed'
+    },
+    logo: {
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 20
+    },
+    card: {
+        paddingTop: 20
+    }
 });
