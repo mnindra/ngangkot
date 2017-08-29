1597414e6a7d7b83391019591e79efb7d007088a
mnindra <mnindrazaka@gmail.com>
2017-08-29 15:18:57 +0700

    validasi form pada saat mendaftar

---
+++ app/components/ErrorLabel.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {
+    Content,
+    Text
+} from 'native-base';
+import { StyleSheet } from 'react-native';
+
+export default class ErrorLabel extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state({
+           error: ""
+        });
+    }
+
+    componentDidMount() {
+        this.setState({
+           error: this.props.errors.split("\n")[0]
+        });
+    }
+
+    render () {
+        return (
+          <Content style={styles.Content}>
+              <Text style={styles.Text}>{this.state.error}</Text>
+          </Content>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+   Content: {
+       margin: 0,
+       paddingTop: 5,
+       paddingBottom: 0,
+       paddingRight: 0,
+       paddingLeft: 15
+   },
+   Text: {
+       color: '#ff4336'
+   }
+});
--- app/screens/Daftar.js
+++ app/screens/Daftar.js
@@ -1,197 +1,211 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ValidationComponent from 'react-native-form-validator';
 import {
-  Container,
-  Content,
-  Button,
-  Text,
-  Form,
-  Item,
-  Input,
-  Card,
-  CardItem,
-  Label,
-  H2,
-  Icon,
-  Thumbnail,
-  StyleProvider
+    Container,
+    Content,
+    Button,
+    Text,
+    Form,
+    Item,
+    Input,
+    Card,
+    H2,
+    Icon,
+    StyleProvider,
+    Label
 } from 'native-base';
 import { StyleSheet, Image } from 'react-native';
+import ErrorLabel from '../components/ErrorLabel';
 import getTheme from '../../native-base-theme/components';
 import material from '../../native-base-theme/variables/material';
 import firebase from '../config/firebase';
 
-export default class Daftar extends Component {
+export default class Daftar extends ValidationComponent {
 
-  constructor (props) {
-    super(props);
-    this.state = {
-      nama: "",
-      email: "",
-      alamat: "",
-      telp: "",
-      password: "",
-      konfirmasi_password: ""
+    constructor (props) {
+        super(props);
+        this.state = {
+            nama: "",
+            email: "",
+            alamat: "",
+            telp: "",
+            password: "",
+            konfirmasi_password: ""
+        }
     }
-  }
 
-  daftar () {
-    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then(() => {
-      let d = new Date();
-      let tanggal = String(d.getDate());
-      let bulan = String(d.getMonth() + 1);
-      let tahun = String(d.getFullYear());
-      let user = firebase.auth().currentUser;
-      firebase.database().ref('penumpang/' + user.uid).set({
-        id_penumpang: user.uid,
-        nama: this.state.nama,
-        email: this.state.email,
-        alamat: this.state.alamat,
-        telp: this.state.telp,
-        password: this.state.password,
-        blokir: 0,
-        lokasi: {
-          latitude: 0,
-          longitude: 0
-        },
-        online: 1,
-        tanggal: `${tanggal}/${bulan}/${tahun}`
-      });
-      this.resetInput();
-    });
-  }
+    daftar () {
+        this.validate({
+            nama: {required: true},
+            email: {required: true},
+            alamat: {required: true},
+            telp: {required: true},
+            password: {required: true}
+        });
 
-  resetInput() {
-    this.setState({
-      nama: "",
-      email: "",
-      alamat: "",
-      telp: "",
-      password: "",
-      konfirmasi_password: ""
-    });
-  }
+        this.forceUpdate();
 
-  render() {
-    const {goBack} = this.props.navigation;
-    return (
-      <StyleProvider style={getTheme(material)}>
-        <Container style={styles.container}>
-          <Content padder>
+        if (this.isFormValid()) {
+            firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then(() => {
+                let d = new Date();
+                let tanggal = String(d.getDate());
+                let bulan = String(d.getMonth() + 1);
+                let tahun = String(d.getFullYear());
+                let user = firebase.auth().currentUser;
+                firebase.database().ref('penumpang/' + user.uid).set({
+                    id_penumpang: user.uid,
+                    nama: this.state.nama,
+                    email: this.state.email,
+                    alamat: this.state.alamat,
+                    telp: this.state.telp,
+                    password: this.state.password,
+                    blokir: 0,
+                    lokasi: {
+                        latitude: 0,
+                        longitude: 0
+                    },
+                    online: 1,
+                    tanggal: `${tanggal}/${bulan}/${tahun}`
+                });
+                this.resetInput();
+            });
+        }
+    }
 
-            <Image style={styles.logo} source={require('../images/logo.png')} />
-            <H2 style={styles.title}>
-              Ngangkot
-            </H2>
+    resetInput() {
+        this.setState({
+            nama: "",
+            email: "",
+            alamat: "",
+            telp: "",
+            password: "",
+            konfirmasi_password: ""
+        });
+    }
 
-            <Card style={styles.card} transparent>
-              <Content padder>
-                <Text style={styles.formText}>Daftar sebagai penumpang baru</Text>
+    render() {
+        const {goBack} = this.props.navigation;
+        return (
+          <StyleProvider style={getTheme(material)}>
+              <Container style={styles.container}>
+                  <Content padder>
+                      <Image style={styles.logo} source={require('../images/logo.png')} />
+                      <H2 style={styles.title}>
+                          Ngangkot
+                      </H2>
 
-                <Form style={styles.form}>
-                  <Item floatingLabel>
-                    <Icon name="person" style={{color:'#4c4c4c'}}/>
-                    <Input
-                      placeholder="Nama"
-                      value={this.state.nama}
-                      onChangeText={(text) => this.setState({nama: text})} />
-                  </Item>
+                      <Card style={styles.card} transparent>
+                          <Content padder>
+                              <Text style={styles.formText}>Daftar sebagai penumpang baru</Text>
 
-                  <Item floatingLabel>
-                    <Icon name="email" style={{color:'#4c4c4c'}}/>
-                    <Input
-                      placeholder="Email"
-                      value={this.state.email}
-                      onChangeText={(text) => this.setState({email: text})}
-                      keyboardType={"email-address"}/>
-                  </Item>
+                              <Form style={styles.form}>
+                                  <Item floatingLabel error={this.isFieldInError('nama')}>
+                                      <Icon name="person" style={{color:'#4c4c4c'}}/>
+                                      <Input
+                                        placeholder="Nama"
+                                        value={this.state.nama}
+                                        onChangeText={(text) => this.setState({nama: text})} />
+                                  </Item>
 
-                  <Item floatingLabel>
-                    <Icon name="location-on" style={{color:'#4c4c4c'}}/>
-                    <Input
-                      placeholder="Alamat"
-                      value={this.state.alamat}
-                      onChangeText={(text) => this.setState({alamat: text})} />
-                  </Item>
+                                  <Item floatingLabel error={this.isFieldInError('email')}>
+                                      <Icon name="email" style={{color:'#4c4c4c'}}/>
+                                      <Input
+                                        placeholder="Email"
+                                        value={this.state.email}
+                                        onChangeText={(text) => this.setState({email: text})}
+                                        keyboardType={"email-address"}/>
+                                  </Item>
 
-                  <Item floatingLabel>
-                    <Icon name="smartphone" style={{color:'#4c4c4c'}}/>
-                    <Input
-                      placeholder="Nomor Telepon"
-                      value={this.state.telp}
-                      onChangeText={(text) => this.setState({telp: text})}
-                      keyboardType={"numeric"}/>
+                                  <Item floatingLabel error={this.isFieldInError('alamat')}>
+                                      <Icon name="location-on" style={{color:'#4c4c4c'}}/>
+                                      <Input
+                                        placeholder="Alamat"
+                                        value={this.state.alamat}
+                                        onChangeText={(text) => this.setState({alamat: text})} />
+                                  </Item>
 
-                  </Item>
-                  <Item floatingLabel>
-                    <Icon name="lock" style={{color:'#4c4c4c'}}/>
-                    <Input
-                      placeholder="Password"
-                      value={this.state.password}
-                      onChangeText={(text) => this.setState({password: text})}
-                      secureTextEntry={true}/>
-                  </Item>
+                                  <Item floatingLabel error={this.isFieldInError('telp')}>
+                                      <Icon name="smartphone" style={{color:'#4c4c4c'}}/>
+                                      <Input
+                                        placeholder="Nomor Telepon"
+                                        value={this.state.telp}
+                                        onChangeText={(text) => this.setState({telp: text})}
+                                        keyboardType={"numeric"}/>
 
-                  <Item floatingLabel>
-                    <Icon name="lock" style={{color:'#4c4c4c'}}/>
-                    <Input
-                      placeholder="Konfirmasi Password"
-                      value={this.state.konfirmasi_password}
-                      onChangeText={(text) => this.setState({konfirmasi_password: text})}
-                      secureTextEntry={true} />
-                  </Item>
-                </Form>
+                                  </Item>
+                                  <Item floatingLabel error={this.isFieldInError('password')}>
+                                      <Icon name="lock" style={{color:'#4c4c4c'}}/>
+                                      <Input
+                                        placeholder="Password"
+                                        value={this.state.password}
+                                        onChangeText={(text) => this.setState({password: text})}
+                                        secureTextEntry={true}/>
+                                  </Item>
 
-                <Button
-                  success
-                  block
-                  onPress={() => this.daftar()}
-                  style={{marginBottom: 10}}>
-                  <Text>Daftar</Text>
-                </Button>
+                                  {/*<Item floatingLabel>*/}
+                                      {/*<Icon name="lock" style={{color:'#4c4c4c'}}/>*/}
+                                      {/*<Input*/}
+                                        {/*placeholder="Konfirmasi Password"*/}
+                                        {/*value={this.state.konfirmasi_password}*/}
+                                        {/*onChangeText={(text) => this.setState({konfirmasi_password: text})}*/}
+                                        {/*secureTextEntry={true} />*/}
+                                  {/*</Item>*/}
+                              </Form>
 
-                <Button
-                  danger
-                  block
-                  onPress={() => goBack()}
-                  style={{backgroundColor: '#ff4336'}}>
-                  <Text>Batal</Text>
-                </Button>
-              </Content>
-            </Card>
-          </Content>
-        </Container>
-      </StyleProvider>
-    );
-  }
+                              <Button
+                                success
+                                block
+                                onPress={() => this.daftar()}
+                                style={{marginBottom: 10}}>
+                                  <Text>Daftar</Text>
+                              </Button>
+
+                              <Button
+                                danger
+                                block
+                                onPress={() => goBack()}
+                                style={{backgroundColor: '#ff4336'}}>
+                                  <Text>Batal</Text>
+                              </Button>
+
+                              <Text>{ this.getErrorMessages() }</Text>
+                          </Content>
+                      </Card>
+                  </Content>
+              </Container>
+          </StyleProvider>
+        );
+    }
 }
+
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#00BCD4'
-  },
-  title: {
-    color: '#fff',
-    textAlign: 'center',
-    marginBottom: 20,
-    fontSize: 30,
-    height: 35
-  },
-  form: {
-    marginBottom: 20
-  },
-  formText: {
-    color: '#5f5f5f',
-    textAlign: 'center'
-  },
-  loginButton: {
-    marginBottom: 10,
-    backgroundColor: '#5c98ed'
-  },
-  logo: {
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginTop: 20
-  },
-  card: {
-    paddingTop: 20
-  }
+    container: {
+        backgroundColor: '#00BCD4'
+    },
+    title: {
+        color: '#fff',
+        textAlign: 'center',
+        marginBottom: 20,
+        fontSize: 30,
+        height: 35
+    },
+    form: {
+        marginBottom: 20
+    },
+    formText: {
+        color: '#5f5f5f',
+        textAlign: 'center'
+    },
+    loginButton: {
+        marginBottom: 10,
+        backgroundColor: '#5c98ed'
+    },
+    logo: {
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 20
+    },
+    card: {
+        paddingTop: 20
+    }
 });
